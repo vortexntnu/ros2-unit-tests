cbc1592ecfe443a67cb6ab441e9dea640cfcbf33
Sondre Haugen <sondre95556888@gmail.com>
2023-10-15 16:46:04 +0200

    First commit

---
+++ build/my_package/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/sondre/ros2_ws/src/install/my_package'
---
+++ my_package/test/test_math.py
@@ -0,0 +1,10 @@
+# import pytest
+
+
+def test_add():
+    assert 3 + 2 == 5
+#    assert 3 + 3 == 5
+
+
+def test_subtract():
+    assert 3 - 2 == 1
